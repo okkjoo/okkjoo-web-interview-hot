31d6718501427f103b19806c2491d056becc6830
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-05-20 09:49:58 +0800

    46.全排列|中等、高频

---
+++ code/46.全排列.js
@@ -0,0 +1,19 @@
+// 回溯函数
+function backtrack(lists, tmpList, nums) {
+	if (tmpList.length === nums.length) return lists.push([...tmpList]);
+	for (let i = 0; i < nums.length; i++) {
+		if (tmpList.includes(nums[i])) continue;
+		tmpList.push(nums[i]);
+		backtrack(lists, tmpList, nums);
+		tmpList.pop(); //回溯
+	}
+}
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+	const lists = [];
+	backtrack(lists, [], nums);
+	return lists;
+};
