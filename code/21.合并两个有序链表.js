27790346199aedc142394fdeb28cb14cade1b82e
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-06-01 11:29:48 +0800

    [21. 合并两个有序链表] 简单|递归|链表

---
+++ code/21.合并两个有序链表.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+	if (list1 === null) return list2;
+	if (list2 === null) return list1;
+	if (list1.val < list2.val) {
+		list1.next = mergeTwoLists(list1.next, list2);
+		return list1;
+	} else {
+		list2.next = mergeTwoLists(list1, list2.next);
+		return list2;
+	}
+};
+// @lc code=end
