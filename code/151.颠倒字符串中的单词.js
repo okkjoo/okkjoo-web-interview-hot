fa6e728455675bce73af21f8d7f915e514b815db
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-08-23 17:23:20 +0800

    151. 颠倒字符串中的单词

---
+++ code/151.颠倒字符串中的单词.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=javascript
+ *
+ * [151] 颠倒字符串中的单词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+	return s.trim().split(/\s+/).reverse().join(' ');
+};
+// @lc code=end
