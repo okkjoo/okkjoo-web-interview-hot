5d6c19bf03b64d1ee9758dc94ba604201b72b63a
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-10-24 11:11:04 +0800

    feat: bind 优化

--- Handwriting/JS basics/bind.js
+++ Handwriting/JS basics/bind.js
@@ -3,15 +3,10 @@
  * @param {any} context 待绑定的上下文
  * @returns 绑定好 this 的函数
  */
-Function.prototype._bind = function (context = globalThis) {
-	const self = this,
-		boundArgs = arguments;
-	return function () {
-		let i = 0,
-			args = [];
-		for (i = 1; i < boundArgs.length; i++) args.push(boundArgs[i]);
-		for (i = 0; i < arguments.length; i++) args.push(arguments[i]);
-		return self.apply(context, args);
+Function.prototype._bind = function (context = globalThis, ...args) {
+	const fn = this;
+	return function (...args2) {
+		return fn.apply(context, args.concat(args2));
 	};
 };
 
@@ -23,3 +18,18 @@ function f(b) {
 f._bind({ a: 3 })(4); //  3, 4
 
 f._bind({ a: 66 }, 10)(11); // 66, 10
+
+//牛客上的测试样例
+function test() {
+	const o1 = {
+		name: 'z',
+		fn: function () {
+			return this.name;
+		},
+	};
+	const o2 = { name: 'g' };
+	const result = o1.fn._bind(o2);
+	return result() === 'g';
+}
+
+console.log(test());
