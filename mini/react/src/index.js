2d31a012ede90e08a9733b8b91110b07b80ec4b4
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-10-27 17:22:50 +0800

    feat: react render

--- mini/react/src/index.js
+++ mini/react/src/index.js
@@ -3,11 +3,10 @@
 		? factory(exports)
 		: typeof define === 'function' && define.amd
 		? define(['exports'], factory)
-		: ((global = global || self), factory((global.Redux = {})));
+		: ((global = global || self), factory((global.Reactz = {})));
 })(this, function (exports) {
 	'use strict';
 	/* createElement */
-
 	function createElement(type, props, ...children) {
 		return {
 			type,
@@ -22,7 +21,7 @@
 
 	/**
 	 *
-	 * @param {string | number} text 基本只
+	 * @param {string | number} text 基本值
 	 * @returns 包裹为 type 为特殊类型 TEXT_ELEMENT 的JS对象
 	 */
 	function createTextElement(text) {
@@ -35,10 +34,32 @@
 		};
 	}
 
-	const Reactz = {
-		createElement,
-		render,
-	};
+	/* ReactDOM.render  */
+	function render(element, container) {
+		//根据 element.type 属性创建 DOM 节点
+		const dom =
+			element.type === 'TEXT_ELEMENT'
+				? document.createTextNode('') //特殊处理 text 节点
+				: document.createElement(element.type);
 
-	exports.Reactz = Reactz;
+		// element props 给到 dom children
+		const isProperty = key => key !== 'children';
+		Object.keys(element.props)
+			.filter(isProperty)
+			.forEach(name => {
+				dom[name] = element.props[name];
+			});
+		//递归处理子节点
+		try {
+			element.props.children.forEach(child => render(child, dom));
+			console.log(element.props);
+		} catch (error) {
+			console.log('!', element.props);
+		}
+		//将节点加入容器中
+		container.appendChild(dom);
+	}
+
+	exports.createElement = createElement;
+	exports.render = render;
 });
